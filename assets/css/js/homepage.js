ad07051ca9069db0d393ad0083e2c88d33ba4887
unknown <temple0715@gmail.com>
2022-06-09 16:21:21 -0500

    update

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -61,10 +61,15 @@ var displayRepos = function(repos, searchTerm) {
     // create a container for each repo
     var repoEl = document.createElement('div');
     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    
 
     // create a span element to hold repository name
     var titleEl = document.createElement('span');
     titleEl.textContent = repoName;
+
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // append to container
     repoEl.appendChild(titleEl);
